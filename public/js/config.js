643e1cbe3c57f8d1b9526892652c1a1bb7b1e2b5
W3Router <info@w3airouter.com>
2024-12-06 21:27:12 +0800

    Fix: update all asset paths to include /public/

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     BASE_URL: window.location.hostname === 'localhost' 
         ? '' 
         : '/eonweb',
-    API_URL: window.location.hostname === 'localhost'
-        ? 'http://localhost:3000'
-        : 'https://你的API域名'
+    ASSETS_URL: window.location.hostname === 'localhost'
+        ? '/public'
+        : '/eonweb/public'
 }; 
